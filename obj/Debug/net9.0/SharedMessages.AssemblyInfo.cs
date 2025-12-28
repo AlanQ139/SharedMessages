f0a3e2199b990a73b86c216bded2aa06ca092156
AlanQ139 <72051493+AlanQ139@users.noreply.github.com>
2025-12-28 15:29:55 -0300

    Add SharedMessages files

--- obj/Debug/net9.0/SharedMessages.AssemblyInfo.cs
+++ obj/Debug/net9.0/SharedMessages.AssemblyInfo.cs
@@ -11,10 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SharedMessages")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FIAP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952a30602e19673a556a21258f39304a1dec42af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b424264f8cc2e963e710a36fd199068f19b85473")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedMessages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedMessages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
